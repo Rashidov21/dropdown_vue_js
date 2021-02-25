daae6ba437b65ff4fbb30563166aaddc1956c491
Rashidov21 <rashidovabdurakhmon@gmail.com>
2021-02-25 14:35:34 +0500

    first commit
    all source code

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  <header>
+    <Navbar/>
+  </header>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+header {
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
+}
+nav{
+
+}
+</style>
